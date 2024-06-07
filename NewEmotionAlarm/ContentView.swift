ea60cdb6319ea1b17ec12c9f72bf333feb3c43f7
Masato <marbo0805@gmail.com>
2024-06-08 01:00:24 +0900

    Initial Commit

---
+++ NewEmotionAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewEmotionAlarm
+//
+//  Created by 深野真人 on 2024/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewEmotionAlarm/NewEmotionAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewEmotionAlarmApp.swift
+//  NewEmotionAlarm
+//
+//  Created by 深野真人 on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct NewEmotionAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
